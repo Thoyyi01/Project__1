d5b89d343bc96bd8184b9c1d4a0d51a475d79a10
Thoyyi01 <164750412+Thoyyi01@users.noreply.github.com>
2025-06-11 05:15:08 +0000

    ArrayDemo1.java

---
+++ ArrayDemo1.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class ArrayDemo1 {
+    static int[] input = new int[10];
+    public static void displayElement() {
+        for(int i=0;i < input.length;i++){
+            System.out.println(input[i]);
+        }
+    }
+    public static void initializeArray(){
+        Scanner scanner = new Scanner(System.in);
+        for(int i=0;i < input.length;i++){
+            System.out.println("Enter Array Element");
+            input[i] = scanner.nextInt();
+        }
+        Scanner.close();
+    }
+    public static void arrayDemo(){
+        //
+        int[] imput = {10,23,34,45,67,78.90};
+        //JAVA 1.5 - For Each Loop
+        for(int i:input){
+            System.out.println(i);
+        }
+    }
+    public static void main(String[] args) {
+        initializeArray();
+        displayElement();
+        arrayDemo();
+    }
+}
